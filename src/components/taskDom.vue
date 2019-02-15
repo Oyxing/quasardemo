20f2793cb94bd6374ff9f7d21785b154e12d890e
Oyxing <897346175@qq.com>
2019-02-15 17:06:36 +0800

    update

--- src/components/taskDom.vue
+++ src/components/taskDom.vue
@@ -1,20 +1,95 @@
 <template>
-<div class="pagecomp ">
+<div class="pagecomp">
+    <div class="pagecompheard">   
+        任务列表
+        <el-button type="warning">批量删除</el-button>
+        <el-button type="primary">创建任务</el-button>
+    </div>
     <div class="showmess" v-loading="dataloading" element-loading-text="正在加载...">
-        <div class="nonebox" v-if="tableData.length <= 0">
-            <img src="statics/img/NOneimg.png" alt="">
-            <p>暂无数据</p>
+        <div class="task" style="margin-top:13px">
+            <el-select v-model="value" placeholder="请选择">
+                <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+            <el-input placeholder="请输入内容" v-model="search" class="input-with-select">
+                <button class="searchbtn" slot="append">
+                <i class="iconfont icon-sousuo"></i>
+                </button>
+            </el-input>
         </div>
-        <div v-else class="showtable keyshow" > 
-            <q-table
-                :data="tableData"
+        <div class="showtable taskshow" style="margin-top:13px"> 
+            <el-table
+                height="200"
+                border
+                :data="tableData.length <= 0?[]:tableData"
+                style="width: 100%"
+                @selection-change="handleSelectionChange"
+                :row-class-name="tableRowClassName">
+                <el-table-column
+                type="selection"
+                width="55">
+                </el-table-column>
+                <el-table-column
+                prop="name"
+                label="任务名称"
+                > 
+                </el-table-column>
+                <el-table-column
+                prop="database"
+                label="数据库名"
+               >  
+                </el-table-column>
+                <el-table-column
+                prop="status"
+                label="状态">
+                 <template slot-scope="scope">
+                    <div>
+                        <span v-if="!scope.row.status">执行中</span>
+                        <span v-else-if="!scope.row.status">执行中</span>
+                        <span v-else-if="scope.row.status == 1">执行中</span>
+                        <span v-else-if="scope.row.status == 2">执行失败</span>
+                        <span v-else-if="scope.row.status == 3">任务完成</span>
+                    </div>
+                </template>
+                </el-table-column>
+                 <el-table-column
+                prop="startime"  
+                label="开始日期">  
+                <template slot-scope="scope">
+                    <div>
+                        {{scope.row.startime}}
+                        <!-- {{timestampToTime(scope.row.startime)}} -->
+                    </div>
+                </template>
+                </el-table-column>
+                 <el-table-column
+                prop="interval"
+                label="周期">
+                </el-table-column>
+                 <el-table-column
+                prop="intime"
+                label="创建日期">
+                <template slot-scope="scope">
+                    <div>
+                        {{scope.row.intime}}
+                        <!-- {{timestampToTime(scope.row.intime)}} -->
+                    </div>
+                </template>
+                </el-table-column>
+            </el-table>
+
+            <!-- <q-table
+                :data="tableData.length <= 0?[]:tableData"
                 :columns="columns"
                 row-key="name"
                 rows-per-page-label="每页行数:"
                 :hide-bottom="hidebottom"
             >
                 <q-td slot="body-cell-intime" slot-scope="props" :props="props">
-                    {{timestampToTime(props.row.intime)}}
                 </q-td>
                 <q-td slot="body-cell-startime" slot-scope="props" :props="props">
                     {{timestampToTime(props.row.startime)}}
@@ -46,7 +121,7 @@
                         @click="props.nextPage"
                         />
                     </div>
-            </q-table>
+            </q-table> -->
         </div>
     </div>
 </div>
@@ -59,71 +134,58 @@ import { userwin,timestampToTime } from 'src/statics/js/public.js'
 export default {
     data() {
         return {
+            search:'',
             userwin:userwin,
             loading:true,
-            dataloading:true,
+            dataloading:false,
             btnnone:false,
             timestampToTime:timestampToTime,
             ipconfig:false,
             hidebottom:true,
             tableData:[
-            ],
-            columns:[ 
                 {
-                name:"1",
-                required: true,
-                label: '任务名称',
-                align: 'center',
-                field: 'name',
-                },
-                {
-                name:"2",
-                required: true,
-                label: '数据库名',
-                align: 'center',
-                field: 'database',
-                },
-                {
-                name:"status",
-                required: true,
-                label: '状态',
-                align: 'center',
-                field: 'status',
-                }, 
-                { 
-                name:"startime",
-                required: true,
-                label: '开始日期',
-                align: 'center',
-                field: 'startime',
-                },
-                {
-                name:"5",
-                required: true,
-                label: '周期',
-                align: 'center',
-                field: 'interval',
-                },
-                {
-                name:"intime",
-                required: true,
-                label: '创建日期',
-                align: 'center',
-                field: 'intime',
-                },
-                {
-                name: 'btn',
-                align: 'center',
-                label: '操作'
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄',
+                }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄',
+                }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
                 }
-            ]
+            ],
+             options: [{
+                value: '选项1',
+                label: '黄金糕'
+                }, {
+                value: '选项2',
+                label: '双皮奶'
+                }, {
+                value: '选项3',
+                label: '蚵仔煎'
+                }, {
+                value: '选项4',
+                label: '龙须面'
+                }, {
+                value: '选项5',
+                label: '北京烤鸭'
+                }],
+                value: ''
         }
     },
     created() {
-        this.tasklist()
-        this.getconfig()
+        // this.tasklist()
+        // this.getconfig()
     },
     mounted() {
+      
         var box = document.getElementById("box")
         var bodybox = document.getElementsByClassName('bodybox')[0]
         var bodyhandlebox = document.getElementsByClassName('bodyhandlebox')[0]
@@ -135,6 +197,18 @@ export default {
         }
     },
     methods:{
+        // 多选
+        handleSelectionChange(val){
+            console.log(val)
+        },
+        tableRowClassName({row, rowIndex}) {
+            if (rowIndex%2) {
+                return 'success-row';
+            } else {
+                return 'warning-row';
+            }
+            return '';
+        },
          keytask:function(){
             var json = {
                 url:'/rsa/getrsakey',
@@ -245,5 +319,52 @@ export default {
 </script>
 
 <style >
-
+    .input-with-select{
+        width: 194px;
+    }
+   .task {
+        margin-top: 13px;
+        width: 380px;
+        float: right;
+        margin-bottom: 13px;
+    }
+    .task .el-select {
+        width: 174px ;
+        height: 28px;
+        margin-right: 10px;
+    }
+    .el-input--suffix .el-input__inner{ 
+        height: 28px;
+    }
+    .task .el-select .el-input__icon{
+        line-height: 28px;
+    }
+    .input-with-select .el-input__inner{
+        width: 152px;
+        height: 28px;
+    }
+    .input-with-select .el-input-group__append{
+        padding: 0 0px;
+        border: 0px solid #dcdfe6;
+    }
+    .input-with-select .el-input-group__append .searchbtn{
+        width: 42px;
+        height: 28px;
+        background: #409EFF;
+        border: none;
+    }
+    .el-table .warning-row {
+        background: #fff;
+    }
+    .el-table .success-row {
+        background: #f7f7f7;
+    }
+    .taskshow .el-table__header-wrapper{
+        height: 30px;
+        line-height: 30px;
+    }
+    .taskshow .el-table--scrollable-y .el-table__body-wrapper .el-table__row{
+        height: 39px;
+    }
+    
 </style>
